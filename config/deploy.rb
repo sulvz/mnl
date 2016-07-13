0ab39593e9ef27d9dd2733eaa487e14d37f82486
Viachaslav Suleimanov <sulvz2000@gmail.com>
2016-07-13 17:07:43 +0300

    Add Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :application, 'mnl24'
+set :repo_url, 'git@github.com:sulvz/mnl24.git'
+
+set :deploy_to, '/home/deploy/mnl24'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
