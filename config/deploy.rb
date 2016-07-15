79f3f5b53a4d852728aba8037c5fffdeda53cab4
Viachaslav Suleimanov <sulvz2000@gmail.com>
2016-07-15 18:57:23 +0300

    Clarify secret_key_base issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:sulvz/mnl24.git'
 
 set :deploy_to, '/home/deploy/mnl24'
 
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{config/database.yml config/secrets.yml .env}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
